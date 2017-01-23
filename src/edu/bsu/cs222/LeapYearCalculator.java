2c44e3c6e4ff544b5bb01b28c5689e1ba32394d6
Logan Kelley <lmkelley2@bsu.edu>
2017-01-22 19:30:29 -0500

    Add tests for centuries and beginning year

--- src/edu/bsu/cs222/LeapYearCalculator.java
+++ src/edu/bsu/cs222/LeapYearCalculator.java
@@ -2,6 +2,13 @@ package edu.bsu.cs222;
 
 public class LeapYearCalculator {
     public boolean isLeapYear(int year) {
-        return true;
+        if (year >= 1752) {
+            if (year % 100 == 0) {
+                return (year % 400 == 0);
+            }
+            return year % 4 == 0;
+        }
+        else
+            return false;
     }
 }
--- test/edu/bsu/cs222/LeapYearCalculatorTest.java
+++ test/edu/bsu/cs222/LeapYearCalculatorTest.java
@@ -16,4 +16,29 @@ public class LeapYearCalculatorTest {
         boolean result = leapYear.isLeapYear(2004);
         Assert.assertTrue(result);
     }
+    @Test
+    public void test2003IsNotLeapYear() {
+        boolean result = leapYear.isLeapYear(2003);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void test1751IsNotLeapYear() {
+        boolean result = leapYear.isLeapYear(1751);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void test1748IsNotLeapYear() {
+        boolean result = leapYear.isLeapYear(1748);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void test1900IsNotLeapYear() {
+        boolean result = leapYear.isLeapYear(1900);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void test2000IsLeapYear() {
+        boolean result = leapYear.isLeapYear(2000);
+        Assert.assertTrue(result);
+    }
 }
